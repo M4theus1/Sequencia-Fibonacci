7653fb33195dde66c23e203978c1895a7af08e62
Matheus Silva <88094156+M4theus1@users.noreply.github.com>
2022-01-03 11:04:07 -0300

    Add files via upload

---
+++ ex.063.py
@@ -0,0 +1,15 @@
+print('Sequência de Fibonacci')
+
+n = int(input('Quantos termos você quer mostrar? '))
+t1 = 0
+t2 = 1
+print('-'*30)
+print('{} -> {}'.format(t1, t2), end= ' ')
+c = 3
+while c <= n:
+    t3 = t1 + t2
+    print('-> {}'.format(t3), end= '-> ')
+    t1 = t2
+    t2 = t3
+    c += 1
+print('FIM')
